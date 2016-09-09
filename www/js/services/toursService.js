f81f06340ec1c11a01a723a1f3ef9343e2db2cfb
Aidan Pine <aidanpine@shaw.ca>
2016-09-09 14:32:06 -0700

    https

--- www/js/services/toursService.js
+++ www/js/services/toursService.js
@@ -2,7 +2,7 @@ angular.module('starter').factory('ToursService', ['$http',
     function($http) {
         return {
             all: function() {
-                return $http.get('http://radhistories.com/wp-content/themes/glwparent-child/tours.php')
+                return $http.get('https://radhistories.com/wp-content/themes/glwparent-child/tours.php')
                     .then(function(response) {
                         return response;
                     });
