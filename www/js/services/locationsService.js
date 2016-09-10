f1f870a4b01141229ff61d2c0f67d512b7b21226
Aidan Pine <aidanpine@shaw.ca>
2016-09-09 21:33:50 -0700

    http

--- www/js/services/locationsService.js
+++ www/js/services/locationsService.js
@@ -2,7 +2,7 @@ angular.module('starter').factory('LocationsService', ['$http',
     function($http) {
         return {
             all: function() {
-                return $http.get('https://radhistories.com/?json=get_recent_posts&post_type=location')
+                return $http.get('http://radhistories.com/?json=get_recent_posts&post_type=location')
                     .then(function(response) {
                         return response;
                     });
